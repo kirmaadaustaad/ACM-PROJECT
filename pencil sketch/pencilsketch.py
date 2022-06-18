59e2ab031b2514df26a7c6e8d6f4aa105c8f904b
kirmaadaustaad <100018817+kirmaadaustaad@users.noreply.github.com>
2022-06-18 22:14:58 +0530

    Update pencilsketch.py

--- pencil sketch/pencilsketch.py
+++ pencil sketch/pencilsketch.py
@@ -1,6 +1,8 @@
+# open cv jindaabaad
 import cv2
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
+# input taken.....$
 
 img = cv2.imread("input1.png")
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -9,6 +11,7 @@ plt.imshow(img)
 plt.axis("off")
 plt.title("Original Image")
 plt.show()
+?#converting thae fileeeee?
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.figure(figsize=(8,8))
@@ -16,7 +19,12 @@ plt.imshow(img_gray,cmap="gray")
 plt.axis("off")
 plt.title("GrayScale Image")
 plt.show()
-
+# rorckkkk
+#    
+# 
+# 
+# 
+# 
 
 img_invert = cv2.bitwise_not(img_gray)
 plt.figure(figsize=(8,8))
@@ -24,17 +32,22 @@ plt.imshow(img_invert,cmap="gray")
 plt.axis("off")
 plt.title("Inverted Image")
 plt.show()
-
+# ok cool nowww
+# just do it
 img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
 plt.figure(figsize=(8,8))
 plt.imshow(img_smoothing,cmap="gray")
 plt.axis("off")
 plt.title("Smoothen Image")
 plt.show()
-
+# final attempt
+# ab to hojaaa yaaaar
 final = cv2.divide(img_gray, 255 - img_smoothing, scale=255)
 plt.figure(figsize=(8,8))
 plt.imshow(final,cmap="gray")
 plt.axis("off")
 plt.title("Final Sketch Image")
 plt.show()
+# plzzz
+# bhagwaan ke liye ho jaa
+# ho gayaaaa
